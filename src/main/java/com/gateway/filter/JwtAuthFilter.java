4d7e476b1104cf1ea341d3d0f14aade4edce3485
Carlos Perez <per.ckarlos@gmail.com>
2026-01-06 16:58:11 -0600

    token file upadted

--- src/main/java/com/gateway/filter/JwtAuthFilter.java
+++ src/main/java/com/gateway/filter/JwtAuthFilter.java
@@ -47,9 +47,9 @@ public class JwtAuthFilter implements WebFilter {
 
                 if (jwtService.isTokenValid(token, userDetails)) {
 
-//                    ServerWebExchange modifiedExchange = exchange.mutate()
-//                            .request(r -> r.header("X-User-Name", username))
-//                            .build();
+                    ServerWebExchange modifiedExchange = exchange.mutate()
+                            .request(r -> r.header("X-User-Name", username))
+                            .build();
 
                     UsernamePasswordAuthenticationToken authToken =
                             new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
