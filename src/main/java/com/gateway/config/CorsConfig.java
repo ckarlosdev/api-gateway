bfa9f110aa8c0f5d6cd2ea9321469af74ccc6d61
Carlos Perez <per.ckarlos@gmail.com>
2026-01-06 13:51:54 -0600

    properties changed to accept github conections

--- src/main/java/com/gateway/config/CorsConfig.java
+++ src/main/java/com/gateway/config/CorsConfig.java
@@ -12,25 +12,16 @@ import java.util.List;
 public class CorsConfig {
     @Bean
     public CorsWebFilter corsWebFilter() {
-        // 1. Crear la fuente de configuración de CORS
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
 
-        // 2. Configurar orígenes permitidos
-        // Permitir el origen de desarrollo de tu frontend de React
-        // ¡Importante! En producción, cambia esto al dominio real (ej: https://tudominio.com)
         config.setAllowedOrigins(List.of("http://localhost:5173", "http://127.0.0.1:5173", "https://oleo-soft.com", "https://ckarlosdev.github.io"));
 
-        // 3. Configurar métodos y encabezados
-        // Permitir todos los métodos HTTP que vas a usar (POST, GET, OPTIONS, etc.)
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
-        // Permitir todos los headers, incluyendo 'Authorization' (para el JWT)
         config.setAllowedHeaders(List.of("*"));
 
-        // 4. Permitir credenciales (importante para enviar cookies/tokens de autenticación)
         config.setAllowCredentials(true);
 
-        // 5. Aplicar esta configuración a todas las rutas (/**)
         source.registerCorsConfiguration("/**", config);
 
         return new CorsWebFilter(source);
